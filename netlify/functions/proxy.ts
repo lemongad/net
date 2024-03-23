cb40e88cb86f4826378af7e71c388b5f03eef0c8
lisa <104255899+lemongad@users.noreply.github.com>
2024-03-23 10:13:45 +0800

    Update proxy.ts

--- netlify/functions/proxy.ts
+++ netlify/functions/proxy.ts
@@ -2,7 +2,7 @@ import { Context } from "netlify:edge";
 
 export default async (request: Request, context: Context) => {
   const url = new URL(request.url);
-  url.hostname = "www.cc.com"; // 替换为您要反向代理的目标网站域名
+  url.hostname = "coze-bot.hf.space";
 
   const proxyRequest = new Request(url.toString(), {
     body: request.body,
