aa314f83441e02c5676cd5d68829807eb22441b9
lisa <104255899+lemongad@users.noreply.github.com>
2024-03-23 10:03:32 +0800

    Update proxy.ts

--- netlify/functions/proxy.ts
+++ netlify/functions/proxy.ts
@@ -1,80 +1,23 @@
-import { Context } from "@netlify/edge-functions";
-
-const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
-  const picked = new Headers();
-  for (const key of headers.keys()) {
-    if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
-      const value = headers.get(key);
-      if (typeof value === "string") {
-        picked.set(key, value);
-      }
-    }
-  }
-  return picked;
-};
-
-const CORS_HEADERS: Record<string, string> = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "*",
-  "access-control-allow-headers": "*",
-};
+import { Context } from "netlify:edge";
 
 export default async (request: Request, context: Context) => {
+  const url = new URL(request.url);
+  url.hostname = "www.cc.com"; // 替换为您要反向代理的目标网站域名
 
-  if (request.method === "OPTIONS") {
-    return new Response(null, {
-      headers: CORS_HEADERS,
-    });
-  }
-
-  const { pathname, searchParams } = new URL(request.url);
-  if(pathname === "/") {
-    let blank_html = `
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>Comedy Central Proxy on Netlify Edge</title>
-</head>
-<body>
-  <h1 id="comedy-central-proxy-on-netlify-edge">Comedy Central Proxy on Netlify Edge</h1>
-  <p>This project uses a reverse proxy to access Comedy Central's website.</p>
-  <p>For more information, please visit <a href="https://www.cc.com">https://www.cc.com</a></p>
-</body>
-</html>
-    `
-    return new Response(blank_html, {
-      headers: {
-        ...CORS_HEADERS,
-        "content-type": "text/html"
-      },
-    });
-  }
-
-  const url = new URL(pathname, "https://www.cc.com");
-  searchParams.delete("_path");
-
-  searchParams.forEach((value, key) => {
-    url.searchParams.append(key, value);
-  });
-
-  const headers = pickHeaders(request.headers, ["content-type", "accept-encoding"]);
-
-  const response = await fetch(url, {
+  const proxyRequest = new Request(url.toString(), {
     body: request.body,
+    headers: request.headers,
     method: request.method,
-    duplex: 'half',
-    headers,
   });
 
-  const responseHeaders = {
-    ...CORS_HEADERS,
-    ...Object.fromEntries(response.headers),
-    "content-content-encoding": null
-  };
+  const response = await fetch(proxyRequest);
+
+  const responseHeaders = new Headers(response.headers);
+  responseHeaders.set("Access-Control-Allow-Origin", "*");
 
   return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
     headers: responseHeaders,
-    status: response.status
   });
 };
